e8b6846d3eae026dc07262c3d0b19cb7cd8c8e79
august110th <80058477+august110th@users.noreply.github.com>
2021-06-25 16:35:16 +0400

    done

--- test_people.py
+++ test_people.py
@@ -1,9 +1,12 @@
 import pytest
-from main import people
-
+from main import people, shelf
 
 
 class Test_buhgalter:
 
-     def test_people(self):
+    def test_people(self):
         assert people("10006") == "Аристарх Павлов"
+    def test_shelf(self):
+        assert shelf('11-2') == '1'
+    def test_(self):
+        assert shelf('11-2') == '1'
